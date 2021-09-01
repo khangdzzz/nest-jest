8a3f54f3842132f5a9ba3b9122cb8676a8059460
lucasrluz <lucasr.luzbr@gmail.com>
2021-08-31 22:54:15 -0300

    Método no service para salvar novo usuário

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entity/user.entity';
+import { IUser } from './interface/user.interface';
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(User) private userRepository: Repository<User>,
   ) {}
+
+  async save(user: IUser) {
+    const userValidate = await this.userRepository.findOne({
+      username: user.username,
+    });
+
+    if (userValidate)
+      throw new InternalServerErrorException({
+        message: 'Este nome de usuário já está em uso',
+      });
+
+    await this.userRepository.insert(user);
+
+    return await this.userRepository.findOne({ username: user.username });
+  }
 }
