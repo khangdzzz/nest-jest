152fa281756d0e9b637ee719d566c3ef1f200e74
lucasrluz <lucasr.luzbr@gmail.com>
2021-08-31 22:12:07 -0300

    Configurações do banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: './database.sqlite',
+      entities: [],
+      synchronize: true,
+      keepConnectionAlive: true,
+    }),
+    UserModule,
+  ],
 })
 export class AppModule {}
